f85d477c856c545279c3703e60da0c1d8a4c6803
Hopeman22 <juantaban@gmail.com>
2020-01-31 15:05:28 +0100

    Actualizaciones de typescript e instalacion del modulo de fire base con las credenciales
    En caso de error probar a hacer la instalacion de firebase y la actualizacion de typescript:
    npm install angularfire2 firebase --save
    npm i typescript@latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from "angularfire2/auth";
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_3BsGcCHTdhKtI_VsH6781E4edtyYwQM",
+    authDomain: "simonapp-46c44.firebaseapp.com",
+    databaseURL: "https://simonapp-46c44.firebaseio.com",
+    projectId: "simonapp-46c44",
+    storageBucket: "simonapp-46c44.appspot.com",
+    messagingSenderId: "126825119697",
+    appId: "1:126825119697:web:e909402df313e377c3eaf0"
+};
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +27,11 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
